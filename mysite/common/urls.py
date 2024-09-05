283d74fcb23bf140aebdcc548b04a992f3adfbab
YooHaJin <yoohajin.dev@gmail.com>
2024-09-05 23:44:33 +0900

    feat: 회원가입

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -6,5 +6,6 @@ app_name = 'common'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
