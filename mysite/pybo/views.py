99c39a8294bf556fd57a0ca5240d8f7c2a8cff7c
YooHaJin <yoohajin.dev@gmail.com>
2024-09-05 21:50:52 +0900

    feat: 페이징 처리

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -8,8 +9,12 @@ from .forms import AnswerForm, QuestionForm
 # Create your views here.
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
 
     return render(request, 'pybo/question_list.html', context)
 
