cd8bb3a263f7cf05d0c421e9b13caef69cf3228c
YooHaJin <yoohajin.dev@gmail.com>
2024-09-05 20:39:14 +0900

    feat: 답변 등록

--- mysite/pybo/forms.py
+++ mysite/pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Question
+from .models import Answer, Question
 
 
 class QuestionForm(forms.ModelForm):
@@ -9,4 +9,13 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변 내용',
         }
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from .forms import QuestionForm
+from .forms import AnswerForm, QuestionForm
 
 
 # Create your views here.
@@ -23,10 +24,21 @@ def detail(request, question_id):
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
-    if request.POST.get('content'):
-        question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
 
-    return redirect('pybo:detail', question_id=question.id)
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+
+    context = {'question': question, 'form': form}
+
+    return render(request, 'pybo/question_detail.html', context)
 
 
 def question_create(request):
