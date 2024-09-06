7b0bbe4525e279682831476ef6353823c1568e55
YooHaJin <yoohajin.dev@gmail.com>
2024-09-06 19:01:58 +0900

    feat: 로그인 상태에서만 글 및 댓글 작성

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.http import HttpResponseNotAllowed
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
@@ -26,6 +26,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
@@ -33,24 +34,27 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
 
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
 
     context = {'question': question, 'form': form}
 
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
 
