07e6fe5218313a17aaafb477a0f99df44b095db5
YooHaJin <yoohajin.dev@gmail.com>
2024-09-09 15:10:27 +0900

    feat: 질문 수정

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,6 @@
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
@@ -62,6 +63,27 @@ def question_create(request):
     else:
         form = QuestionForm()
 
+    context = {'form': form}
+
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
     context = {'form': form}
 
     return render(request, 'pybo/question_form.html', context)
